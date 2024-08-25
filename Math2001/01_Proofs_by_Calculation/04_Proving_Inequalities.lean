67bf565346c9c1e3cedd3144fc7adbf4862e6570
zaqqwerty <12915808+zaqqwerty@users.noreply.github.com>
2024-08-25 01:00:45 -0400

    done with main text of chapter 1

--- Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
+++ Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
@@ -76,7 +76,8 @@ example {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
     _ >= 5 * n := by rel [hn]
     _ = 2 * n + 3 * n := by ring
     _ >= 2 * n + 3 * 5 := by rel [hn]
-    _ >= 2 * n + 11 := by numbers
+    _ = 2 * n + 15 := by ring
+    _ > 2 * n + 11 := by addarith [hn]
 
 
 -- Example 1.4.7
